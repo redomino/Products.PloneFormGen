82ddbaa5b6e3d922d7b944503cd55f17df09fa0c
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2012-03-30 18:31:33 +0200

    Update of the Italian i18n: start\!

--- Products/PloneFormGen/content/form.py
+++ Products/PloneFormGen/content/form.py
@@ -68,7 +68,7 @@ FormFolderSchema = ATFolderSchema.copy() + Schema((
         languageIndependent=1,
         widget=BooleanWidget(label=_(u'label_showcancel_text',
                                      default=u'Show Reset Button'),
-            description=_(u'help_showcancel_text'),
+            description=_(u'help_showcancel_text', default=u""),
             ),
         ),
     StringField('resetLabel',
